8a191d6bd0616c5fe6d4add9fe0be39b9807a4e6
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2022-02-04 19:52:43 +0000

    please someone contribute to this pleaseeeeee
    god damn please someone bloody help this terrible mess

--- KnifeBot.lua
+++ KnifeBot.lua
@@ -3,30 +3,9 @@
 -- hello kat devs :)
 -- if you are even here.
 
--- nolag
-loadstring(game:HttpGet('https://raw.githubusercontent.com/cOpEtriNe/Katware-/main/NoLag.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/cOpEtriNe/Katware-/main/mousePosPrint.lua'))()
+-- scripts
+-- go get them, edit them, do whatever with them. Just make sure to give credit.
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/cOpEtriNe/Katware-/30dd1189a8aeb23e188a696efd962e269250b073/ChatSpam.lua'))()
-
-while true do 
-wait(0.5)
-mouse1click()
-wait(0.5)
-mouse1click() 
-wait(0.5)
-mouse1click()
-wait(0.5)
-mouse1click() 
-wait(0.5)
-mouse1click()
-wait(0.5)
-mouse1click()
-wait(0.5)
-mouse1click()
-wait(0.5)
-mouse1click() -- max
-end
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/cOpEtriNe/Katware-/main/EquipNoob.lua'))()
-
 loadstring(game:HttpGet('https://raw.githubusercontent.com/BoatRollerz/special-disco/main/KATSlient.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/cOpEtriNe/Katware-/main/virtualClick.lua'))()
