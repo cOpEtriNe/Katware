6ede31645fb446e94a43b41a0486c97f366685c7
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2022-02-05 13:11:12 +0000

    Fix

--- EquipNoob.lua
+++ EquipNoob.lua
@@ -1,3 +1,13 @@
+
+
+local args = {
+    [1] = "NoobKnife"
+}
+
+game:GetService("ReplicatedStorage").GameEvents.Misk.ItemPurchase:FireServer(unpack(args))
+
+-- equips
+
 local args = {
     [1] = "Equip",
     [2] = "6",
@@ -9,6 +19,13 @@ game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(
 wait(1)
 
 -- equip the noob pax
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+	Title = "Kat GUI"; -- the title (ofc)
+	Text = "Alert!, - You need to be level 15 to get the noob knife! Ignore this message if you over level 15 or are level 15!"; -- what the text says (ofc)
+	Icon = "rbxassetid://57254792"; -- the image if u want. 
+	Duration = 5; -- how long the notification should in secounds
+})
 
 local args = {
     [1] = "Equip",
