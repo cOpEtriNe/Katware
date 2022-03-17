37f927a09d1ad34c28352443f4e453698c9b0f43
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2022-03-17 20:37:21 +0000

    Delete mousePosPrint.lua

--- mousePosPrint.lua
+++
@@ -1,8 +0,0 @@
-local Players = game:GetService("Players")
- 
-local localPlayer = Players.LocalPlayer
-local mouse = localPlayer:GetMouse()
-    
-mouse.Move:Connect(function()
-	print("KATWare Mouse Position Current/Before: ", mouse.X, mouse.Y)
-end)
