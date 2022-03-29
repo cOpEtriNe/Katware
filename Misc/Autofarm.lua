878260ff56027818cd418d387a3518c7679fce28
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2022-03-29 19:57:55 +0100

    fix

--- Misc/Autofarm.lua
+++ Misc/Autofarm.lua
@@ -1,12 +1,13 @@
 -- for the pasters
-	while true do
+while true do
+wait(0.01)
+		for i,v in pairs(game.Players:GetChildren()) do
+		if v ~= game.Players.LocalPlayer then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,-1)
 		wait(0.01)
-			for i,v in pairs(game.Players:GetChildren()) do
-				if v ~= game.Players.LocalPlayer then
-					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,-1)
-					wait(1)
-					mouse1click()
-					wait(0.01)
-				end
-			end
+		mouse1click()
+		wait(0.01)
+	
 		end
+	end
+end
