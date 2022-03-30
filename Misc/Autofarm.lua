f9aeaa28232374ecef8ebda05365b768ef234fa0
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2022-03-30 18:39:54 +0100

    bruh

--- Misc/Autofarm.lua
+++ Misc/Autofarm.lua
@@ -1,4 +1,4 @@
-repeat until false
+while true do
 wait(1)
 for i,v in pairs(game.Players:GetChildren()) do
     if v ~= game.Players.LocalPlayer then
@@ -7,6 +7,5 @@ for i,v in pairs(game.Players:GetChildren()) do
          wait(0.1)
          game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-7, 958, -36)
     end
-end
 end
 end
