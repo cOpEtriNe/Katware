3eee25963564652acb2855375334d950a386bec7
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2022-03-30 17:39:03 +0100

    fix crash

--- Misc/Autofarm.lua
+++ Misc/Autofarm.lua
@@ -1,11 +1,11 @@
-repeat until false
-wait(5)
+while true do
+wait(0.01)
 for i,v in pairs(game.Players:GetChildren()) do
-     if v ~= game.Players.LocalPlayer then
-          game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,-1)
-          wait(0.1)
-          mouse1click()
-          wait(0.1)
-     end
+    if v ~= game.Players.LocalPlayer then
+         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.Head.CFrame * CFrame.new(0,0,.4)
+         mouse1click()
+         wait(0.1)
+         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-7, 958, -36)
+    end
 end
 end
