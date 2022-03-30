e50d5ed8071de2c9ebea518339226fc52c34c54d
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2022-03-30 17:47:43 +0100

    It works now (I think)

--- Misc/Autofarm.lua
+++ Misc/Autofarm.lua
@@ -1,5 +1,5 @@
-while true do
-wait(0.01)
+repeat until false
+wait(1)
 for i,v in pairs(game.Players:GetChildren()) do
     if v ~= game.Players.LocalPlayer then
          game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.Head.CFrame * CFrame.new(0,0,.4)
@@ -7,5 +7,6 @@ for i,v in pairs(game.Players:GetChildren()) do
          wait(0.1)
          game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-7, 958, -36)
     end
+end
 end
 end
