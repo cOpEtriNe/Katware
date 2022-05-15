3916d9f93002474b502127067be0bd3c415f88d2
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2022-05-15 18:57:14 +0100

    yes
    yes true

---
+++ Misc/changer.lua
@@ -0,0 +1,178 @@
+-- created by cOpEtriNe
+-- thihs was added here because of some stupid reason with gui 2 lua
+script.Parent.MouseButton1Click:Connect(function()
+	-- Hello.
+	while true do
+		wait(0.5)
+		local args = {
+			[1] = "Equip",
+			[2] = "72",
+			[3] = 1
+		}
+
+		game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	
+	local args = {
+		[1] = "Equip",
+		[2] = "71",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "68",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+
+	local args = {
+		[1] = "Equip",
+		[2] = "85",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "73",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "77",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "82",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "80",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "78",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "6",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "70",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "69",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "77",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "50",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "75",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "84",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "83",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+
+	local args = {
+		[1] = "Equip",
+		[2] = "79",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "76",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "74",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+	wait(0.5)
+	local args = {
+		[1] = "Equip",
+		[2] = "81",
+		[3] = 1
+	}
+
+	game:GetService("ReplicatedStorage").GameEvents.Misk.InventoryChange:FireServer(unpack(args))
+end
+end)
